9f56067692217767533f9205b7e461e41396ef79
Aidan <awan2344@lps53.org>
2022-10-02 15:01:53 -0500

    Finished Shooter

--- src/main/java/frc/robot/subsystems/shooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooterSubsystem.java
@@ -4,13 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.RobotConstants;
-import frc.robot.libraries.external.control.PidConstants;
 import frc.robot.libraries.internal.LazyTalonFX;
-
+import frc.robot.constants.ShooterConstants;
 public class shooterSubsystem extends SubsystemBase {
   /** Creates a new shooterSubsystem. */
 private LazyTalonFX shooterMasterMotor; 
@@ -18,27 +18,43 @@ private LazyTalonFX shooterFollowMotor;
 
 public double shooterVelocity = 0;
 
-  public shooterSubsystem() {
-    shooterMasterMotor = new LazyTalonFX(RobotConstants.SHOOTER_MASTER_MOTOR)
-    shooterFollowMotor = new LazyTalonFX(RobotConstants.SHOOTER_FOLLOWER_MOTOR)
+  public shooterSubsystem(double Velocity) {
+    shooterMasterMotor =ConfigureMotors(RobotConstants.SHOOTER_MASTER_MOTOR, true, false);
+    shooterFollowMotor = ConfigureMotors(RobotConstants.SHOOTER_FOLLOWER_MOTOR, false, false);
 
-
+    shooterVelocity = Velocity;
   }
 
+private LazyTalonFX ConfigTalons(int portNum, boolean isMaster, boolean isInverted){
+
+  LazyTalonFX talon = new LazyTalonFX(portNum);
+  talon.configFactoryDefault();
+  talon.setInverted(isInverted);
+  talon.enableVoltageCompensation(true);
+  talon.configVoltageCompSaturation(12.0, ShooterConstants.kTimeoutMs);
+
+  if(isMaster){
+    talon.configOpenloopRamp(ShooterConstants.openDriveVoltageRampRate);
+  }
+
+  return talon;
+}
+
 public LazyTalonFX ConfigureMotors(int portNum, boolean isMaster, boolean isInverted){
-  LazyTalonFX talon = configTalons(portNum, isMaster, isInverted);
+  LazyTalonFX talon = ConfigTalons(portNum, isMaster, isInverted);
   talon.setNeutralMode(NeutralMode.Coast);
 
   if(isMaster){
-  talon.config_kP(PidConstants.kSlot_Shooter_Velocity, 0.060, PidConstants.kTimeoutMs);
-  talon.config_kI(PidConstants.kSlot_Shooter_Velocity, 0.0, PidConstants.kTimeoutMs);
-  talon.config_kD(PidConstants.kSlot_Shooter_Velocity, 0.0009, PidConstants.kTimeoutMs);
-  talon.config_kF(PidConstants.kSlot_Shooter_Velocity, 0.049, PidConstants.kTimeoutMs);
-  talon.config_IntegralZone(PidConstants.kSlot_Shooter_Velocity, (int)PidConstants.kGains_Velocity_Shooter.kIzone, PidConstants.kTimeoutMs);
-  talon.configClosedLoopPeakOutput(PidConstants.kSlot_Shooter_Velocity, PidConstants.kGains_Velocity_Shooter.kPeakOutput, PidConstants.kTimeoutMs);
-  talon.configAllowableClosedloopError(PidConstants.kSlot_Shooter_Velocity, 0, PidConstants.kTimeoutMs);
-  talon.selectProfileSlot(PidConstants.kSlot_Shooter_Velocity, PidConstants.PID_PRIMARY);
+  talon.config_kP(ShooterConstants.kSlot_Shooter_Velocity, 0.060, ShooterConstants.kTimeoutMs);
+  talon.config_kI(ShooterConstants.kSlot_Shooter_Velocity, 0.0, ShooterConstants.kTimeoutMs);
+  talon.config_kD(ShooterConstants.kSlot_Shooter_Velocity, 0.0009, ShooterConstants.kTimeoutMs);
+  talon.config_kF(ShooterConstants.kSlot_Shooter_Velocity, 0.049, ShooterConstants.kTimeoutMs);
+  talon.config_IntegralZone(ShooterConstants.kSlot_Shooter_Velocity, (int)ShooterConstants.kGains_Velocity_Shooter.kIzone, ShooterConstants.kTimeoutMs);
+  talon.configClosedLoopPeakOutput(ShooterConstants.kSlot_Shooter_Velocity, ShooterConstants.kGains_Velocity_Shooter.kPeakOutput, ShooterConstants.kTimeoutMs);
+  talon.configAllowableClosedloopError(ShooterConstants.kSlot_Shooter_Velocity, 0, ShooterConstants.kTimeoutMs);
+  talon.selectProfileSlot(ShooterConstants.kSlot_Shooter_Velocity, ShooterConstants.PID_PRIMARY);
   }
+  return talon;
 }
 
   @Override
@@ -47,6 +63,16 @@ public LazyTalonFX ConfigureMotors(int portNum, boolean isMaster, boolean isInve
   }
 
   public void Shoot(){
-
+shooterMasterMotor.set(ControlMode.Velocity, shooterVelocity);
   }
+
+  public void stopShooter(){
+    shooterMasterMotor.set(ControlMode.PercentOutput,0);
+  
+  }
+
+  public void setShooterVelocity(double velocity) {
+    shooterVelocity = velocity;
+  }
+
 }
